9930470a3800d80834197b969f32b8adcbd585a6
László Kővári <maximuse@windowslive.com>
2018-01-12 01:43:40 +0100

    Initial commit

---
+++ src/hu/nyirszikszi/Main.java
@@ -0,0 +1,10 @@
+package hu.nyirszikszi;
+
+import hu.nyirszikszi.penzvalto.Penzvalto;
+
+public class Main {
+    public static void main(String[] args) {
+        Penzvalto p = new Penzvalto(14500);
+        p.felvalt();
+    }
+}
---
+++ src/hu/nyirszikszi/penzvalto/Penzvalto.java
@@ -0,0 +1,33 @@
+package hu.nyirszikszi.penzvalto;
+
+public class Penzvalto {
+    private int penz;
+
+    public Penzvalto(int penz) {
+        this.penz = penz;
+    }
+
+    public int getPenz() {
+        return penz;
+    }
+
+    public void setPenz(int penz) {
+        this.penz = penz;
+    }
+
+    public void felvalt() {
+        int[] cimletek = {5000, 2000, 1000, 500};
+        String eredmeny = "";
+        
+        for (int cimlet : cimletek) {
+            if(this.penz / cimlet != 0) {
+                int db = 0;
+                db = this.penz / cimlet;
+                this.penz -= db * cimlet;
+                eredmeny += db + "\t*\t" + cimlet + "\nbent van:\t" + this.penz + "\n";
+            }
+        }
+
+        System.out.println(eredmeny);
+    }
+}
