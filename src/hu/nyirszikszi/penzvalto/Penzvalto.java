5ea9715487f19f302159acc7b12398448c020544
László Kővári <maximuse@windowslive.com>
2018-01-12 21:15:28 +0100

    Initial commit

--- src/hu/nyirszikszi/penzvalto/Penzvalto.java
+++ src/hu/nyirszikszi/penzvalto/Penzvalto.java
@@ -19,11 +19,14 @@ public class Penzvalto {
         String eredmeny = "bent van:\t" + getPenz() + "\n";
 
         for (int cimlet : cimletek) {
-            if(this.penz / cimlet != 0) {
+            if(this.penz / cimlet != 0 && (cimlet==5000 || cimlet==2000 || cimlet==1000 || cimlet==500)) {
                 int db = 0;
                 db = this.penz / cimlet;
                 this.penz -= db * cimlet;
                 eredmeny += db + "\t*\t" + cimlet + "\nbent van:\t" + getPenz() + "\n";
+            }
+            else {
+                eredmeny += cimlet + " címletet nem lehet kikérni\n";
             }
         }
 
